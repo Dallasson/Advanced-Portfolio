7b5b104e32871793806678221bd1352cbb789490
Dallasson <eddinetaki087@gmail.com>
2025-09-13 18:02:27 +0100

    new changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    "eslint.enable": false,
     ignores: [
       "node_modules/**",
       ".next/**",
